06354f44702d650523c81345231d48c8b9bd6bca
perez.jacob <perez.jacob@SEMS-259-30.SEMS259.LAB>
2018-10-24 16:27:15 -0700

    tuday i did minion test

---
+++ src/Minion.java
@@ -0,0 +1,61 @@
+
+public class Minion{
+	
+	private String name; 
+ 	private int eyes; 
+ private String color; 
+ 	private String master;
+	
+		
+
+ 	public Minion(String name, int eyes, String color, String master) {
+		this.name=name;
+		this.eyes=eyes;
+		this.color=color;
+		this.master=master;
+		// TODO Auto-generated constructor stub
+
+
+	}
+	String getName() {
+		return name;
+	}
+	int getEyes(){
+		return eyes;
+	}
+	
+	String getColor() {
+		return color;
+	}
+	
+	String getMaster(){
+		return master;
+	
+		
+	}
+	
+	void setName(String name) {
+		this.name=name;
+	}
+  void setEyes(int eyes) {
+	  this.eyes=eyes;
+  }
+  
+  void setColor (String color) {
+	  this.color=color;
+  }
+void setMaster(String master ) {
+	this.master=master;
+}
+	}
+
+	
+
+ 
+	
+	
+		
+	
+
+
+
